effe0f59a890e1debbebc406e05d5269d2579021
root <root@localhost.localdomain>
2025-04-17 02:12:28 -0400

    Trigger CI/CD via NGrok Tunnel

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask - DevOps Test! And Now it is Updated now you can see it because i updated it without downtime"
+    return "Hahahahahahahahahaha"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
